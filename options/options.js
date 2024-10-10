2a668dbde69d5ff1c118b100b77a30573369a7b5
Juan Cruz <juan@crowdbotics.com>
2024-10-10 14:31:02 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "jnmtest-49150",
-  url: "https://jnmtest-49150.botics.co",
-  api: "https://jnmtest-49150.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "jnmtest-49150",
+  "url": "https://jnmtest-49150.botics.co",
+  "api": "https://jnmtest-49150.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "react_native_2fa"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
