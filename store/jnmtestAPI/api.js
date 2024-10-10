8229dcd788a209316dd659754f7a68cfa2e1ca95
Juan Cruz <juan@crowdbotics.com>
2024-10-10 14:27:29 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/jnmtestAPI/api.js
+++ store/jnmtestAPI/api.js
@@ -1,86 +1,120 @@
-import axios from "axios"
+import axios from "axios";
 const jnmtestAPI = axios.create({
   baseURL: "https://jnmtest-49150.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
-  return jnmtestAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return jnmtestAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_dog_list(payload) {
-  return jnmtestAPI.get(`/api/v1/dog/`)
+  return jnmtestAPI.get(`/api/v1/dog/`);
 }
+
 function api_v1_dog_create(payload) {
-  return jnmtestAPI.post(`/api/v1/dog/`, payload)
+  return jnmtestAPI.post(`/api/v1/dog/`, payload);
 }
+
 function api_v1_dog_retrieve(payload) {
-  return jnmtestAPI.get(`/api/v1/dog/${payload.id}/`)
+  return jnmtestAPI.get(`/api/v1/dog/${payload.id}/`);
 }
+
 function api_v1_dog_update(payload) {
-  return jnmtestAPI.put(`/api/v1/dog/${payload.id}/`, payload)
+  return jnmtestAPI.put(`/api/v1/dog/${payload.id}/`, payload);
 }
+
 function api_v1_dog_partial_update(payload) {
-  return jnmtestAPI.patch(`/api/v1/dog/${payload.id}/`, payload)
+  return jnmtestAPI.patch(`/api/v1/dog/${payload.id}/`, payload);
 }
+
 function api_v1_dog_destroy(payload) {
-  return jnmtestAPI.delete(`/api/v1/dog/${payload.id}/`)
+  return jnmtestAPI.delete(`/api/v1/dog/${payload.id}/`);
 }
+
 function api_v1_hbcbd_list(payload) {
-  return jnmtestAPI.get(`/api/v1/hbcbd/`)
+  return jnmtestAPI.get(`/api/v1/hbcbd/`);
 }
+
 function api_v1_hbcbd_create(payload) {
-  return jnmtestAPI.post(`/api/v1/hbcbd/`, payload)
+  return jnmtestAPI.post(`/api/v1/hbcbd/`, payload);
 }
+
 function api_v1_hbcbd_retrieve(payload) {
-  return jnmtestAPI.get(`/api/v1/hbcbd/${payload.id}/`)
+  return jnmtestAPI.get(`/api/v1/hbcbd/${payload.id}/`);
 }
+
 function api_v1_hbcbd_update(payload) {
-  return jnmtestAPI.put(`/api/v1/hbcbd/${payload.id}/`, payload)
+  return jnmtestAPI.put(`/api/v1/hbcbd/${payload.id}/`, payload);
 }
+
 function api_v1_hbcbd_partial_update(payload) {
-  return jnmtestAPI.patch(`/api/v1/hbcbd/${payload.id}/`, payload)
+  return jnmtestAPI.patch(`/api/v1/hbcbd/${payload.id}/`, payload);
 }
+
 function api_v1_hbcbd_destroy(payload) {
-  return jnmtestAPI.delete(`/api/v1/hbcbd/${payload.id}/`)
+  return jnmtestAPI.delete(`/api/v1/hbcbd/${payload.id}/`);
 }
+
 function api_v1_login_create(payload) {
-  return jnmtestAPI.post(`/api/v1/login/`, payload)
+  return jnmtestAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return jnmtestAPI.post(`/api/v1/signup/`, payload)
+  return jnmtestAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/login/`, payload)
+  return jnmtestAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/logout/`)
+  return jnmtestAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/password/change/`, payload)
+  return jnmtestAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/password/reset/`, payload)
+  return jnmtestAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return jnmtestAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/registration/`, payload)
+  return jnmtestAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_resend_email_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/registration/resend-email/`, payload)
+  return jnmtestAPI.post(`/rest-auth/registration/resend-email/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return jnmtestAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return jnmtestAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return jnmtestAPI.get(`/rest-auth/user/`)
+  return jnmtestAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return jnmtestAPI.put(`/rest-auth/user/`, payload)
+  return jnmtestAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return jnmtestAPI.patch(`/rest-auth/user/`, payload)
+  return jnmtestAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_dog_list,
@@ -108,4 +142,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
